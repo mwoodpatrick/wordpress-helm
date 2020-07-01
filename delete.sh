b5e2f4116521b2aaad220179a9e397b4ef808724
Maarten de Waard <maarten@greenhost.nl>
2020-07-01 17:42:30 +0200

    remove subPath, fix comments, remove tmp directory after using it

--- delete.sh
+++ delete.sh
@@ -9,8 +9,8 @@ helm delete $releaseName
 
 ## Delete remaining PVCs:
 kubectl delete pvc \
-	data-$releaseName-database-master-0 \
-	data-$releaseName-database-slave-0 \
-	redis-data-$releaseName-redis-master-0
-    redis-data-$releaseName-redis-slave-0
+    data-$releaseName-database-master-0 \
+    data-$releaseName-database-slave-0 \
+    redis-data-$releaseName-redis-master-0 \
+    redis-data-$releaseName-redis-slave-0 \
     redis-data-$releaseName-redis-slave-1
