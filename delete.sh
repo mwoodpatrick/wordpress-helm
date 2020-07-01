1d6270dea4d71eab231febee0eaeb1f7b40017da
Maarten de Waard <maarten@greenhost.nl>
2020-07-01 16:56:25 +0200

    process some feedback

--- delete.sh
+++ delete.sh
@@ -12,3 +12,5 @@ kubectl delete pvc \
 	data-$releaseName-database-master-0 \
 	data-$releaseName-database-slave-0 \
 	redis-data-$releaseName-redis-master-0
+    redis-data-$releaseName-redis-slave-0
+    redis-data-$releaseName-redis-slave-1
