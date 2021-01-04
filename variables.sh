db6df374aac6c2881eb4ecde3bc1d50f4c4c43ef
Maarten de Waard <maarten@greenhost.nl>
2021-01-04 17:20:44 +0100

    cut branch names to 63 characters in variables.sh

--- variables.sh
+++ variables.sh
@@ -1,5 +1,5 @@
 # Set the docker image tag to the current branch:
-dockerTag=$(git rev-parse --abbrev-ref HEAD)
+dockerTag=$(git rev-parse --abbrev-ref HEAD | cut -c "-63")
 
 # Only get the number from the issue (or whatever is in front of a dash
 # otherwise) for the release name
