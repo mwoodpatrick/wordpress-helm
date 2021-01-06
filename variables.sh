025e17e0dc4f053826bb884a74b29983cfbd2682
Maarten de Waard <maarten@greenhost.nl>
2021-01-06 16:27:24 +0000

    Apply 1 suggestion(s) to 1 file(s)

--- variables.sh
+++ variables.sh
@@ -1,4 +1,6 @@
-# Set the docker image tag to the current branch:
+# Set the docker image tag to the name of the current branch.
+# Shortened to 63 bytes because Gitlab does the same with the `CI_COMMIT_REF_SLUG`
+# variable, which we use in the CI script to build the docker image.
 dockerTag=$(git rev-parse --abbrev-ref HEAD | cut -c "-63")
 
 # Only get the number from the issue (or whatever is in front of a dash
