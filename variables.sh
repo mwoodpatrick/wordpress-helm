0a24069c4e4592d27d974419bddceea30323912e
Maarten de Waard <maarten@greenhost.nl>
2021-01-06 17:33:32 +0100

    actually cut to 63 bytes, not characters

--- variables.sh
+++ variables.sh
@@ -1,7 +1,7 @@
 # Set the docker image tag to the name of the current branch.
 # Shortened to 63 bytes because Gitlab does the same with the `CI_COMMIT_REF_SLUG`
 # variable, which we use in the CI script to build the docker image.
-dockerTag=$(git rev-parse --abbrev-ref HEAD | cut -c "-63")
+dockerTag=$(git rev-parse --abbrev-ref HEAD | cut -b "-63")
 
 # Only get the number from the issue (or whatever is in front of a dash
 # otherwise) for the release name
